299a97cd6a05e3db8ac78d8bcae57d5f5a52a406
Ilya <ilya.tyunin.2457@gmail.com>
2026-02-21 22:16:18 +0300

    fix: небольшие правки

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://tomato1337.site',
     output: 'server',
     adapter: node({
         mode: 'standalone',
